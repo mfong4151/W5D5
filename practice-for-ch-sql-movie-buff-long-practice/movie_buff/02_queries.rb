f04e7761e693420e05a266a748e608378df926fc
mfong415 <85459923+mfong415@users.noreply.github.com>
2022-11-04 17:11:59 -0700

    First commit

---
+++ practice-for-ch-sql-movie-buff-long-practice/movie_buff/02_queries.rb
@@ -0,0 +1,46 @@
+def eighties_b_movies
+  # List all the movies from 1980-1989 with scores falling between 3 and 5
+  # (inclusive). Show the id, title, year, and score.
+  #Movie.where(yr: 1980..1989).where(score: 3..5).select(:id, :title, :yr, :score) 
+
+  Movie.where(yr: 1980..1989, score: 3..5).select(:id, :title, :yr, :score) 
+  #Movie.where('yr BETWEEN 1980 AND 1989 and score BETWEEN 3 and 5').select(:id, :title, :yr, :score) 
+
+end
+
+def bad_years
+  # List the years in which no movie with a rating above 8 was released.
+  Movie.group(:yr).having("MAX(movies.score) <= 8").pluck(:yr)
+end
+
+def cast_list(title)
+  # List all the actors for a particular movie, given the title.
+  # Sort the results by starring order (ord). Show the actor id and name.
+  
+  Actor.select(:id, :name).joins(:movies).where('movies.title = ?', title).order('castings.ord asc')
+end
+
+def vanity_projects
+  # List the title of all movies in which the director also appeared as the
+  # starring actor. Show the movie id, title, and director's name.
+
+  #director_id = actor_id and ord =1
+  # Note: Directors appear in the 'actors' table.
+
+  Movie.joins(:actors).where("director_id = actor_id AND ord = 1").select(:id,:title,:name)
+  
+end
+
+def most_supportive
+  # Find the two actors with the largest number of non-starring roles. .having(Count(ord))
+  # Show each actor's id, name, and number of supporting roles.
+
+  Actor
+  .joins(:castings)
+  .select(:id, :name, 'COUNT(*) AS roles')
+  .where('ord != 1')
+  .group(:id)
+  .limit(2)
+  .order('roles desc')
+
+end
---
+++ queries.rb
@@ -0,0 +1,90 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  title       :string           not null
+#  yr          :integer          not null
+#  score       :float            not null
+#  votes       :integer          not null
+#  director_id :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Table name: castings
+#
+#  id         :bigint           not null, primary key
+#  movie_id   :bigint           not null
+#  actor_id   :bigint           not null
+#  ord        :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+
+
+def find_angelina
+  # find Angelina Jolie by name in the actors table
+  angelina = Actor.where('name = ?', 'Angelina Jolie')
+  angelina[0]
+end
+
+def top_titles
+  # get movie titles from movies with scores greater than or equal to 9
+  # hint: use 'select' and 'where'
+  top = Movie.select(:title).where('score >= ?', 9)
+  top
+end
+
+def star_wars
+  # display the id, title, and year of each Star Wars movie in movies
+  # hint: use 'select' and 'where'
+  sw = Movie.select(:id, :title, :yr).where("title LIKE ?",'Star Wars%')
+end
+
+
+def below_average_years
+  # display each year with movies scoring under 5, with the count of movies
+  # scoring under 5 aliased as bad_movies, in descending order
+  # hint: use 'select', 'where', 'group', 'order'
+  
+  below_average = Movie.select(:yr, 'COUNT(score) AS bad_movies').where('score < 5').group('yr').order('bad_movies asc')
+end
+
+def alphabetized_actors
+  # display the id and name of the second 10 actor names (i.e., #s 11-20)
+  # when ordered from A-Z
+  # hint: use 'order' and 'limit'
+  
+  actors = Actor.select(:id, :name).order('name Asc').limit('10').offset('10')
+
+end
+
+def pulp_fiction_actors
+  # practice using joins
+  # display the id and name of all actors in the movie Pulp Fiction
+  # hint: use 'select', 'joins', 'where'
+  #this means we have to join on all three tables
+
+  actors = Actor.joins(:castings).joins(:movies).select(:id, :name).where('title = ?', 'Pulp Fiction').uniq
+
+end
+
+def uma_movies
+  # practice using joins
+  # display the id, title, and year of movies Uma Thurman has acted in
+  # order them by ascending year
+  # hint: use 'select', 'joins', 'where', and 'order'
+
+  actors = Movie.joins(:castings).joins(:actors).select(:id, :title, :yr).where('actors.name = ?', 'Uma Thurman').uniq
+  
+end
+
+
+#Find how many likes each chirp has 
+
